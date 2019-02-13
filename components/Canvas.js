0824a5e91c85e82559265e77ede9e90f68d03edd
sthobis <s.thobi.sinaga@gmail.com>
2019-02-13 12:58:15 +0700

    feat: add enemy, basic collision detection and scoring

--- components/Canvas.js
+++ components/Canvas.js
@@ -4,6 +4,7 @@ const CANVAS_SIZE = 400;
 const BLOCK_SIZE = 20;
 const SHIP_SIZE = 20;
 const BULLET_SIZE = 10;
+const ENEMY_SIZE = 16;
 const CANVAS_DIMENSION = CANVAS_SIZE / BLOCK_SIZE;
 const OUT_OF_MAP = { x: -1, y: -1 };
 
@@ -86,7 +87,7 @@ const Canvas = () => {
 
   const mouse = useMousePosition(canvasRef);
 
-  const [shipCoordinate, setShipCoordinate] = useState({ x: 0, y: 0 });
+  const [shipCoordinate, setShipCoordinate] = useState(OUT_OF_MAP);
   useEffect(
     () => {
       const x = Math.floor(mouse.x / CANVAS_DIMENSION);
@@ -103,22 +104,72 @@ const Canvas = () => {
     setBulletCoordinate({ x: shipCoordinate.x, y: shipCoordinate.y - 1 });
     setIsShooting(true);
   };
-  const stopBulletInterval = useInterval(
+  useInterval(
     () => {
       if (bulletCoordinate.y === 0) {
-        stopBulletInterval();
         setBulletCoordinate(OUT_OF_MAP);
         setIsShooting(false);
       } else {
-        setBulletCoordinate(prev => {
-          return {
-            x: prev.x,
-            y: prev.y - 1
-          };
-        });
+        setBulletCoordinate(prev => ({
+          x: prev.x,
+          y: prev.y - 1
+        }));
       }
     },
     isShooting ? 100 : null
+  );
+
+  const [enemyCoordinate, setEnemyCoordinate] = useState(OUT_OF_MAP);
+  const [isEnemyAlive, setIsEnemyAlive] = useState(false);
+  useInterval(() => {
+    setEnemyCoordinate({
+      x: Math.floor(Math.random() * CANVAS_DIMENSION),
+      y: 0
+    });
+    setIsEnemyAlive(true);
+  }, 2200);
+  useInterval(
+    () => {
+      if (enemyCoordinate.y === CANVAS_DIMENSION) {
+        setEnemyCoordinate(OUT_OF_MAP);
+        setIsEnemyAlive(false);
+        setScore(prev => prev - 200);
+      } else {
+        setEnemyCoordinate(prev => ({
+          x: prev.x,
+          y: prev.y + 1
+        }));
+      }
+    },
+    isEnemyAlive ? 100 : null
+  );
+
+  const [score, setScore] = useState(0);
+  useEffect(
+    () => {
+      if (
+        shipCoordinate.x === enemyCoordinate.x &&
+        shipCoordinate.y === enemyCoordinate.y &&
+        shipCoordinate !== OUT_OF_MAP
+      ) {
+        setScore(prev => prev - 1000);
+        setEnemyCoordinate(OUT_OF_MAP);
+        setIsEnemyAlive(false);
+      }
+
+      if (
+        bulletCoordinate.x === enemyCoordinate.x &&
+        bulletCoordinate.y === enemyCoordinate.y &&
+        bulletCoordinate !== OUT_OF_MAP
+      ) {
+        setScore(prev => prev + 100);
+        setBulletCoordinate(OUT_OF_MAP);
+        setIsShooting(false);
+        setEnemyCoordinate(OUT_OF_MAP);
+        setIsEnemyAlive(false);
+      }
+    },
+    [bulletCoordinate, enemyCoordinate]
   );
 
   const drawCanvas = () => {
@@ -126,11 +177,12 @@ const Canvas = () => {
     ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
     drawShip(ctx);
     drawBullet(ctx);
+    drawEnemy(ctx);
   };
   useAnimationFrame(drawCanvas);
 
   const drawShip = ctx => {
-    ctx.fillStyle = "#ccc";
+    ctx.fillStyle = "#00f";
     ctx.fillRect(
       BLOCK_SIZE * shipCoordinate.x + (BLOCK_SIZE - SHIP_SIZE) / 2,
       BLOCK_SIZE * shipCoordinate.y + (BLOCK_SIZE - SHIP_SIZE) / 2,
@@ -149,17 +201,34 @@ const Canvas = () => {
     );
   };
 
+  const drawEnemy = ctx => {
+    ctx.fillStyle = "#f00";
+    ctx.fillRect(
+      BLOCK_SIZE * enemyCoordinate.x + (BLOCK_SIZE - ENEMY_SIZE) / 2,
+      BLOCK_SIZE * enemyCoordinate.y + (BLOCK_SIZE - ENEMY_SIZE) / 2,
+      ENEMY_SIZE,
+      ENEMY_SIZE
+    );
+  };
+
   return (
     <div className="canvas-container">
+      <p>
+        Hook Invaders
+        <br />
+        <a href="https://github.com/sthobis/hook-invaders">source</a>
+      </p>
       <canvas
         ref={canvasRef}
         width={CANVAS_SIZE}
         height={CANVAS_SIZE}
         onClick={shootBullet}
       />
+      <p>score: {score}</p>
       <style jsx>{`
         .canvas-container {
           display: flex;
+          flex-direction: column;
           justify-content: center;
           align-items: center;
           width: 100vw;
@@ -168,6 +237,16 @@ const Canvas = () => {
 
         canvas {
           background: #000;
+        }
+
+        canvas:hover {
+          cursor: none;
+        }
+
+        p {
+          font-family: monospace;
+          font-size: 14px;
+          text-align: center;
         }
       `}</style>
       <style jsx global>{`
